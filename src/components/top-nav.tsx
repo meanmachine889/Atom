f95ecb1424e8de067c93410d839b3597cbeb1357
meanmachine889 <ybharadwaj131@gmail.com>
2025-03-30 19:48:56 +0530

    fixes

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -1,8 +1,5 @@
 "use client"
-
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Bell, ChevronRight } from "lucide-react"
-import Profile01 from "./profile-01"
 import Link from "next/link"
 
 interface BreadcrumbItem {
@@ -39,26 +36,6 @@ export default function TopNav() {
         >
           <Bell className="h-4 w-4 sm:h-5 sm:w-5 text-gray-600 dark:text-gray-300" />
         </button>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none">
-            {/*eslint-disable-next-line @next/next/no-img-element*/}
-            <img
-              src="https://ferf1mheo22r9ira.public.blob.vercel-storage.com/avatar-01-n0x8HFv8EUetf9z6ht0wScJKoTHqf8.png"
-              alt="User avatar"
-              width={28}
-              height={28}
-              className="rounded-full ring-2 ring-gray-200 dark:ring-[#2B2B30] sm:w-8 sm:h-8 cursor-pointer"
-            />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent
-            align="end"
-            sideOffset={8}
-            className="w-[280px] sm:w-80 bg-background border-border rounded-lg shadow-lg"
-          >
-            <Profile01 avatar="https://ferf1mheo22r9ira.public.blob.vercel-storage.com/avatar-01-n0x8HFv8EUetf9z6ht0wScJKoTHqf8.png" />
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </nav>
   )
